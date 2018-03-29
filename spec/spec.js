f590623f0543bf8989df8394103d52788eafd639
samuel <samuel@vastdata.com>
2018-03-29 05:04:29 +0300

    Adding tests

--- spec/spec.js
+++ spec/spec.js
@@ -6,7 +6,7 @@ let page;
 
 describe('Setup' , function () {
     beforeAll((async function () {
-        browser = await puppeteer.launch({headless: true});
+        browser = await puppeteer.launch({headless: false});
         page = await browser.newPage();
         await page.setViewport({width, height});
     }));
@@ -17,14 +17,27 @@ describe('Setup' , function () {
         it("Daily Lesson - Filter \"Topics\"", async function () {
 
             await page.goto('https://archive.kbb1.com/lessons');
+
             let headerHandle = await page.$('.section-header');
-
             let headerClassName = await page.evaluate(header => header.className, headerHandle);
-
             expect(headerClassName).toBe('section-header');
+
+            headerHandle = await page.$('.section-header__title');
+            let headerText = await page.evaluate(header => header.innerHTML, headerHandle);
+            expect(headerText).toBe('Daily Lessons');
+            headerHandle.dispose()
 
         });
 
+        fit("Daily Lesson - Pagination", async function () {
+
+            await page.goto('https://archive.kbb1.com/lessons');
+
+            let headerHandle = await page.$('.ui.blue.compact.pagination-menu.menu');
+            let paginationClassName = await page.evaluate(header => header.className, headerHandle);
+            expect(paginationClassName).toBe('ui blue compact pagination-menu menu');
+
+        });
     });
 
     afterAll(async function () {
