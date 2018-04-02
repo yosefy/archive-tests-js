1e3ec3537dc924cabae293966ea0f11e63a5ef16
samuel <samuel@vastdata.com>
2018-04-02 05:48:25 +0300

    Fix: timeout issues

--- spec/spec.js
+++ spec/spec.js
@@ -3,10 +3,14 @@ const width = 1920;
 const height = 1080;
 let browser;
 let page;
-const lessonsUrl = 'https://archive.kbb1.com/lessons'
+let originalTimeout;
+const lessonsUrl = 'https://archive.kbb1.com/lessons';
+const lessonsPlayerUrl = 'https://archive.kbb1.com/lessons/cu/uyFMxrLN';
 
 describe('Setup' , function () {
     beforeAll((async function () {
+        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
         browser = await puppeteer.launch({headless: false});
         page = await browser.newPage();
         await page.setViewport({width, height});
@@ -20,20 +24,27 @@ describe('Setup' , function () {
             // header
             expect(await page.$('.section-header')).toBeDefined();
             // header title
-            expect(await page.$eval('.section-header__title',(selector) => {return selector.innerHTML})).toBe('Daily Lessons');
+            expect(await page.$eval('.section-header__title',
+                (selector) => {return selector.innerHTML})).toBe('Daily Lessons');
             // filters
-            expect(await page.$$eval('.ui.container.padded.horizontally a', (selector) => {return selector.length})).toBe(3);
+            expect(await page.$$eval('.ui.container.padded.horizontally a',
+                (selector) => {return selector.length})).toBe(3);
         });
 
         it('Daily Lesson - Pagination', async function () {
             await page.goto(lessonsUrl);
             expect(await page.$eval('.ui.blue.compact.pagination-menu.menu',
                 (selector) => {return selector.className})).toBe('ui blue compact pagination-menu menu');
+        });
 
+        it('Daily Lesson - Player', async function () {
+            await page.goto(lessonsPlayerUrl);
+            expect(await page.$('.mediaplayer')).toBeDefined();
         });
     });
 
     afterAll(async function () {
         await browser.close();
+        jasmine.DEFAULT_TIMEOUT_INTERVAL = originalTimeout;
     });
 });
