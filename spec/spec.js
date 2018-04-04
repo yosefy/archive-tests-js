0b3587a0e66ecb990fec9bd7cbe8e2d88a495469
Benyamin Rafaeli <benyamin.r@fintier.com>
2018-04-04 05:27:39 +0300

    added try catch structure

--- spec/spec.js
+++ spec/spec.js
@@ -1,3 +1,4 @@
+
 const puppeteer = require('puppeteer');
 const width = 1920;
 const height = 1080;
@@ -7,13 +8,17 @@ let originalTimeout;
 const lessonsUrl = 'https://archive.kbb1.com/lessons';
 const lessonsPlayerUrl = 'https://archive.kbb1.com/ua/lessons/cu/TGfvHRww?language=en';
 
-describe('Setup' , function () {
+describe('Setup', function () {
     beforeAll((async function () {
-        originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
-        jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
-        browser = await puppeteer.launch({headless: false});
-        page = await browser.newPage();
-        await page.setViewport({width, height});
+        try {
+            originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
+            jasmine.DEFAULT_TIMEOUT_INTERVAL = 10000;
+            browser = await puppeteer.launch({headless: false});
+            page = await browser.newPage();
+            await page.setViewport({width, height});
+        } catch (err) {
+            expect(err.status).toBeGreaterThanOrEqual(200);
+        }
     }));
 
 
@@ -25,16 +30,22 @@ describe('Setup' , function () {
             expect(await page.$('.section-header')).toBeDefined();
             // header title
             expect(await page.$eval('.section-header__title',
-                (selector) => {return selector.innerHTML})).toBe('Daily Lessons');
+                (selector) => {
+                    return selector.innerHTML
+                })).toBe('Daily Lessons');
             // filters
             expect(await page.$$eval('.ui.container.padded.horizontally a',
-                (selector) => {return selector.length})).toBe(3);
+                (selector) => {
+                    return selector.length
+                })).toBe(3);
         });
 
         it('Daily Lesson - Pagination', async function () {
             await page.goto(lessonsUrl, {waitUntil: 'domcontentloaded'});
             expect(await page.$eval('.ui.blue.compact.pagination-menu.menu',
-                (selector) => {return selector.className})).toBe('ui blue compact pagination-menu menu');
+                (selector) => {
+                    return selector.className
+                })).toBe('ui blue compact pagination-menu menu');
         });
 
         it('Daily Lesson - Player', async function () {
